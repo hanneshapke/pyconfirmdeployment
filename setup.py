864d2f24c8ddf312ae5e31ff46f212953aa10b48
Hannes Hapke <hannes.hapke@gmail.com>
2016-01-12 12:46:50 -0800

    updated console_scripts

--- setup.py
+++ setup.py
@@ -38,5 +38,7 @@ setup(
     install_requires=install_requires,
     depedency_links=dependency_links,
     author_email='hannes.hapke@gmail.com',
-    entry_points={'console_scripts': ['%(package)s=%(package)s.%(package)s:main' % {'package': __package__}]},
+    entry_points={
+        'console_scripts': ['pyconfirmdeployment = pyconfirmdeployment.confirm:run_pyconfirmdeployment', ]},
+    # entry_points={'console_scripts': ['%(package)s=%(package)s.%(package)s:main' % {'package': __package__}]},
 )
