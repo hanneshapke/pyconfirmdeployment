38952b4934adb1e490eab68b8ddc7a793b8bf3e3
Hannes Hapke <hannes.hapke@gmail.com>
2016-01-12 13:11:45 -0800

    Added __main__

---
+++ __main__.py
@@ -0,0 +1,32 @@
+import sys
+import argparse
+from pyconfirmdeployment.confirm import get_confirmation
+
+
+def main(args=None):
+    """The main routine."""
+    if args is None:
+        args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser(
+        description=(
+            'Checks webservers after a deployment and emails '
+            'the given email addresses a screenshot')
+        )
+    parser.add_argument(
+        '-u', '--url', help='URL to check',
+        required=True)
+    parser.add_argument(
+        '-t', '--to', help='Receiving emails of the screenshot',
+        required=True)
+    parser.add_argument(
+        '-f', '--fromemail', help='From email address',
+        required=True)
+
+    args = parser.parse_args()
+    sys.exit(get_confirmation(
+        args.url, to_emails=[args.to, ], from_email=args.fromemail,)
+    )
+
+if __name__ == "__main__":
+    main()
